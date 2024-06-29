432057223c052b40f30d3b44289bb7137cc7bc68
Jaime Yu <jaimelyu@gmail.com>
2024-06-28 23:15:40 -0400

    Set up to take initial temp and salinity and visualize them

--- saltftnsMCDR.jl
+++ saltftnsMCDR.jl
@@ -1,8 +1,13 @@
 using Pkg
 using Oceananigans
 using OceanBioME
-using TimeDates
+using TimesDates
+using CairoMakie
 
+#pipe parameters
+pipe_radius = 0.5;
+pipe_length = 10;
+pipe_top_depth = 1;
 
 #resolution parameters TODO determine best ones
 #note: be careful of time resolution needed, spacing resoslution needed
@@ -37,6 +42,44 @@ timestepper = :QuasiAdamsBashforth2; #default, 3rd order option available
 #following are determinined automatically: pressure solver 
 #have yet to think about following: auxiliary fields
 model = NonhydrostaticModel(; grid, clock, advection, buoyancy, tracers, timestepper)
+
+#setting up just a basic gradient for water column 
+T_top = 21.67;
+T_bot = 11.86;
+S_bot = 35.22;
+S_top = 34.18;
+delta_z = 200;
+#sets up initial gradient, starting from surface
+#TODO: pipe, can use for loop to set up mask, or somehow find a mask
+T_initial(x, z) = T_top - ((T_bot - T_top)/delta_z)z;
+S_initial(x, z) = S_top - ((S_bot - S_top)/delta_z)z;
+set!(model, T = T_initial; S = S_initial)
+
+
+#visualize inital temperature distribution
+fig = Figure()
+title = "initial conditions"
+Label(fig[0, :], title)
+
+axis_kwargs = (xlabel = "x (m)", ylabel = "z (m)", width = 400)
+ax1 = Axis(fig[1, 1]; title = "Temperature (C)", axis_kwargs...)
+ax2 = Axis(fig[2, 1]; title = "Salinity (ppt)", axis_kwargs...)
+
+T_lims = (T_initial(0, -domain_z), T_top);
+S_lims = (S_top, S_initial(0, -domain_z));
+x = xnodes(model.tracers.T)
+z = znodes(model.tracers.T)
+T_init_matrix = interior(model.tracers.T, :, 1, :)
+S_init_matrix = interior(model.tracers.S, :, 1, :)
+hm1 = heatmap!(ax1, x , z, T_init_matrix, colorrange = T_lims, colormap = :thermal, interpolate = true)
+hm2 = heatmap!(ax2, x , z, S_init_matrix, colorrange = S_lims, colormap = :haline, interpolate = true)
+Colorbar(fig[1, 2], hm1)
+Colorbar(fig[2, 2], hm2)
+
+fig 
+
+#visualize initial salinity distribution
+
 
 #setting initial conditions 
 #TODO: perturb one part, maybe give it an intial velocity from pumping?? (this would align with matlab)
