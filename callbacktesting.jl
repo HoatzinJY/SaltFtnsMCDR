85b3751665de885bfd537b7568ef3fc18146d3cc
Jaime Yu <jaimelyu@gmail.com>
2024-07-19 18:25:02 -0700

    testing using last delta t

--- callbacktesting.jl
+++ callbacktesting.jl
@@ -27,7 +27,7 @@ end
 #FIGURE OUT WHAT A DIFFUSIVITY FIEDLD IS? 
 
 function maskCenter(x)
-    if (1 < x < 2)
+    if (1 <= x <= 2)
         return 1
     else
         return 0
@@ -52,18 +52,29 @@ closure = ScalarDiffusivity(ν=0, κ = 1.0)
 A_bcs = FieldBoundaryConditions(east = ValueBoundaryCondition(10), west = ValueBoundaryCondition(1))
 boundary_conditions = (A = A_bcs,)
 
-function A_forcing_func(x, t, A, time_step)
-    @info @sprintf("In function, x = %.2f | A = %.2f | ΔT = %s", x, A, prettytime(time_step))
-    return maskCenter(x) * 1/(1.01*time_step) * (0 - A)
-end 
+#not set up right now takes in wrong step
+# function A_forcing_func(x, t, A, stepping::NamedTuple)
+#     @info @sprintf("In function, x = %.2f | A = %.2f | ΔT = %s", x, A, prettytime(time_step))
+#     return maskCenter(x) * 1/(1.01*stepping[:forcing_rate]()) * (0 - A)
+# end 
+
+function test_forcing_func(fieldNodes ,i, j, k, velocity_field, clock)
+    #currently max increase set to 1.01, so i set this to 1.015
+    @info @sprintf("In function, x = %.2f | A = %.2f | ΔT = %s", x, A, prettytime(clock.last_Δt))
+    return @inbounds maskCenter(fieldNodes[1][i]) * 1/(1.015 *clock.last_Δt) *(0 - velocity_field[i, j, k])
+end
+
 
 diffusion_time_scale = (1^2)/1
 initial_time_step = 0.1*diffusion_time_scale 
 time_step = initial_time_step
-A_forcing = Forcing(A_forcing_func, parameters=time_step, field_dependencies=:A)
+tracer_field_nodes = nodes(grid, (Center(), Center(), Center()); reshape = true)
+A_forcing_func(i, j, k, grid, clock, model_fields) = test_forcing_func(tracer_field_nodes, i, j, k, model_fields.A, clock)
+A_forcing = Forcing(A_forcing_func, discrete_form = true)
+#A_forcing = Forcing(A_forcing_func, parameters=rate_tuple, field_dependencies=:A)
 forcing = (A = A_forcing,)
-
 model = NonhydrostaticModel(; grid, clock, timestepper, tracers, closure, boundary_conditions, forcing)
+
 set!(model, A = A_init)
 @info "initial conditions set"
 plotInitialConditions(model)
@@ -73,19 +84,26 @@ num_iterations = 3
 run_duration = 30
 simulation = Simulation(model, Δt = initial_time_step, stop_iteration = num_iterations, wall_time_limit = run_duration)
 timeWizard = TimeStepWizard(cfl=0.2, diffusive_cfl = 0.2) 
-# function timeWizardMessage()
-#     @info @sprintf("*****WIZARD CALLED*****, Δt = %s", prettytime(sim.Δt))
-#     timeWizard
-#     @info @sprintf("*****WIZARD ENDED*****, Δt = %s", prettytime(sim.Δt))
-# end
+function timeWizardMessage(sim)
+    @info @sprintf("*****WIZARD CALLED*****, Δt = %s", prettytime(sim.Δt))
+    TimeStepWizard(cfl=0.2, diffusive_cfl = 0.2)(sim)
+    @info @sprintf("*****WIZARD ENDED*****, Δt = %s", prettytime(sim.Δt))
+end
 #TODO: work on gettng those to call time 
-simulation.callbacks[:timeWizard] = Callback(timeWizard, IterationInterval(1))
+simulation.callbacks[:timeWizard] = Callback(timeWizardMessage, IterationInterval(1))
+
+# function updateRelaxation!(sim)
+#     model = sim.model
+#     model.forcing.A = 
+# end
 progress_message_timestep(sim) = @printf("TIMESTEP Iteration: %04d, time: %.3e seconds, Δt: %s\n", iteration(sim), time(sim), prettytime(sim.Δt))
-progress_message_tendency(sim) = @printf("TENDENCY Iteration: %04d, time: %.3e seconds, Δt: %s\n", iteration(sim), time(sim), prettytime(sim.Δt))
-progress_message_updatestate(sim) = @printf("UPDATESTATE Iteration: %04d, time: %s, Δt: %s\n", iteration(sim), prettytime(sim), prettytime(sim.Δt))
-add_callback!(simulation, progress_message_tendency, IterationInterval(1), callsite = TendencyCallsite())
-add_callback!(simulation, progress_message_updatestate, IterationInterval(1), callsite = UpdateStateCallsite())
+#progress_message_tendency(model) = @printf("TENDENCY Iteration: %04d, time: %.3e seconds, Δt: %s\n", iteration(sim), time(sim), prettytime(sim.Δt))
+#progress_message_updatestate(model) = @printf("UPDATESTATE Iteration: %04d, time: %s, Δt: %s\n", iteration(sim), prettytime(sim), prettytime(sim.Δt))
+#add_callback!(simulation, progress_message_tendency, IterationInterval(1), callsite = TendencyCallsite())
+#add_callback!(simulation, progress_message_updatestate, IterationInterval(1), callsite = UpdateStateCallsite())
 add_callback!(simulation, progress_message_timestep, IterationInterval(1), callsite = TimeStepCallsite())
+#This next callback MUST BE ADDED IN ORDER AFTER THE TIMEWIZARD IS ADDED
+
 
 
 # iteration(simulation)
@@ -124,3 +142,7 @@ record(fig, filename * "video.mp4", frames, framerate=1) do i
     @info string("Plotting frame ", i, " of ", frames[end])
     n[] = i
 end
+
+
+clock.last_Δt
+clock
